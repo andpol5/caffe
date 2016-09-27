5e5b79c19c0ef178ddd58e5254e71a815de27f80
Andrei Polzounov <and.pol5@gmail.com>
2016-09-27 17:03:09 +0800

    Simplify the ImageLabelDataLayer to not use strided labels

--- include/caffe/layers/image_label_data_layer.hpp
+++ include/caffe/layers/image_label_data_layer.hpp
@@ -20,7 +20,6 @@ template<typename Dtype>
 class ImageLabelDataLayer : public BasePrefetchingDataLayer<Dtype> {
  public:
   explicit ImageLabelDataLayer(const LayerParameter &param);
-
   virtual ~ImageLabelDataLayer();
 
   virtual void DataLayerSetUp(const vector<Blob<Dtype> *> &bottom,
@@ -30,32 +29,28 @@ class ImageLabelDataLayer : public BasePrefetchingDataLayer<Dtype> {
   virtual inline bool ShareInParallel() const { return false; }
 
   virtual inline const char *type() const { return "ImageLabelData"; }
-
   virtual inline int ExactNumBottomBlobs() const { return 0; }
-
-  virtual inline int ExactNumTopBlobs() const { return -1; }
-
-  virtual inline int MaxTopBlobs() const { return 3; }
-
-  virtual inline int MinTopBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 2; }
 
  protected:
   shared_ptr<Caffe::RNG> prefetch_rng_;
 
   virtual void ShuffleImages();
-
-  virtual void SampleScale(cv::Mat *image, cv::Mat *label);
-
   virtual void load_batch(Batch<Dtype>* batch);
 
-  vector<std::string> image_lines_;
-  vector<std::string> label_lines_;
+  vector<std::pair<std::string, std::string> > lines_;
   int lines_id_;
 
-  Blob<Dtype> transformed_label_;
+  int batch_size_;
+  int crop_size_;
 
-  int label_margin_h_;
-  int label_margin_w_;
+  std::string image_dir_;
+  std::string label_dir_;
+  vector<int> image_shape_;
+  vector<int> label_shape_;
+
+  Blob<Dtype> transformed_data_;
+  Blob<Dtype> transformed_label_;
 
   std::mt19937 *rng_;
 };
